afe02f09cc10848e1ff84f59df7e2209b839fa46
DMingOu <dmingou0529@gmail.com>
2020-04-05 00:52:06 +0800

    移动了文件MainActivity

--- app/src/main/java/com/qg/sh_data_app/ui/MainActivity.java
+++ app/src/main/java/com/qg/sh_data_app/ui/MainActivity.java
@@ -1,9 +1,10 @@
-package com.qg.sh_data_app;
+package com.qg.sh_data_app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.qg.sh_data_app.R;
 import com.qg.sh_data_app.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
